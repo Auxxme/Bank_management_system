95b4017d80d5a4ef34a69c67ddb7a3318598e29e
Auxxme <90456467+Auxxme@users.noreply.github.com>
2021-09-10 17:57:07 +0530

    Create Welcome.cpp

---
+++ Welcome.cpp
@@ -0,0 +1,81 @@
+# Bank_management_system
+
+#include<iostream>
+#include<conio.h>
+#include<windows.h>
+#include<unistd.h>
+using namespace std;
+void welcome();
+void jio();
+
+
+
+
+  void welcome()
+  {
+
+  cout<<"\n\n\n\n\n\n\n ";
+  system("color C");
+
+  cout<<"\t\t\t\t\t\t                                                                                                    "<<endl;
+  cout<<"\t\t\t\t\t\t                                                                                                    "<<endl;
+  cout<<"\t\t\t\t\t\t                                                                                                   "<<endl;
+  cout<<"\t\t\t\t\t\t                                                                                                    "<<endl;
+  cout<<"\t\t\t\t\t\t                                                                                                    "<<endl;
+  cout<<"\t\t\t\t\t\t                                                                                                "<<endl;
+  cout<<"\t\t\t\t\t\t                                                                                                    "<<endl;
+  cout<<"\t\t\t\t\t\t   ***                     **  *******   **         ********  ********* ***          *** ******                  "<<endl;
+  cout<<"\t\t\t\t\t\t    ***        **         **   **        **         **        *       * ** *        * ** *                 "<<endl;
+  cout<<"\t\t\t\t\t\t     ***      ** **      **    **        **         **        *       * **  *      *  ** *               " <<endl;
+  cout<<"\t\t\t\t\t\t      ***    **  **     **     *******   **         **        *       * **   *    *   ** ******            "<<endl;
+  cout<<"\t\t\t\t\t\t       ***   **   **   **      **        **         **        *       * **    *  *    ** *             "<<endl;
+  cout<<"\t\t\t\t\t\t        *** **     ** **       **        **         **        *       * **     **     ** *            "<<endl;
+  cout<<"\t\t\t\t\t\t         ****       ***        *******   ********** ********  ********* **     *      ** *******            "<<endl;
+  cout<<endl;
+  Sleep(1100);
+  system("color A");
+  cout<<endl<<endl<<endl<<endl<<endl<<endl<<endl;
+  cout<<"\t\t\t\t\t\t                                                           "<<endl;
+  cout<<"\t\t\t\t\t\t                                                           "<<endl;
+  cout<<"\t\t\t\t\t\t                                                            "<<endl;
+  cout<<"\t\t\t\t\t\t                                                            "<<endl;
+  cout<<"\t\t\t\t\t\t                             *************   **********                            "<<endl;
+  cout<<"\t\t\t\t\t\t                             *************   **********      "<<endl;
+  cout<<"\t\t\t\t\t\t                                   **        *        *        "<<endl;
+  cout<<"\t\t\t\t\t\t                                   **        *        *           "<<endl;
+  cout<<"\t\t\t\t\t\t                                   **        *        *      "<<endl;
+  cout<<"\t\t\t\t\t\t                                   **        *        *            "<<endl;
+  cout<<"\t\t\t\t\t\t                                   **        * ****** *       "<<endl;
+  cout<<"\t\t\t\t\t\t                                   **        **********                   "<<endl;
+  Sleep(1050);
+  system("color 4");
+  cout<<endl<<endl<<endl<<endl<<endl;
+  jio();
+  Sleep(1050);
+  system("color 5E");
+  Sleep(1050);
+  system("color 1F");
+
+  }
+
+ void jio()
+ {
+  system("cls");
+  system("COLOR 0A");
+  cout<<"\t\t\t\t\t\t                                                                                                    "<<endl;
+  cout<<"\t\t\t\t\t\t                                                                                                    "<<endl;
+  cout<<"\t\t\t\t\t\t                                                                                                    "<<endl;
+  cout<<"\t\t\t\t\t\t                                                                                                     "<<endl;
+  cout<<"\t\t\t\t\t\t                                                                                                    "<<endl;
+  cout<<"\t\t\t\t\t\t                                                                                                    "<<endl;
+  cout<<"\t\t\t\t\t\t                                                                                                    "<<endl;
+  cout<<"\t\t\t\t\t\t           *********  **  ********* \t        ***********          **       ***         ** **                           "<<endl;
+  cout<<"\t\t\t\t\t\t               *      **  *       * \t        *        **         ***       ** **       ** **                 "<<endl;
+  cout<<"\t\t\t\t\t\t               *      **  *       * \t        *        **        ** **      **  **      ** **  **                "<<endl;
+  cout<<"\t\t\t\t\t\t               *      **  *       * \t        *        **       **   **     **   **     ** ** *                     "<<endl;
+  cout<<"\t\t\t\t\t\t               *      **  *       * \t        *  ******        *********    **    **    ** ***                            "<<endl;
+  cout<<"\t\t\t\t\t\t               *      **  *       * \t        *        **     **       **   **     **   ** ****             "<<endl;
+  cout<<"\t\t\t\t\t\t        **     *      **  *       * \t        *        **    **         **  **      ****** **  *                          "<<endl;
+  cout<<"\t\t\t\t\t\t        ********      **  ********* \t        ***********   **           ** **       ***** **   **                          "<<endl;
+
+ }
